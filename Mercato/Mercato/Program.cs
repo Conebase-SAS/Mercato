35c500cf7b4ef2e6ba4cbf299840366f4a21dada
Lens Mutombo <lensmutombo@gmail.com>
2022-06-23 01:45:38 +0200

    Update Program.cs

--- Mercato/Mercato/Program.cs
+++ Mercato/Mercato/Program.cs
@@ -17,7 +17,7 @@ namespace Mercato
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frm_shop   ());
+            Application.Run(new frm_categories());
         }
     }
 }
