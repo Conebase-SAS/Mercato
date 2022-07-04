6cf48a122a7f8b3a62a85cf41e7351775cf03bc8
Lens Mutombo <lensmutombo@gmail.com>
2022-07-04 15:30:15 +0200

    Added some designs

---
+++ Mercato/Mercato/Views/UserControls/uc_clients.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mercato.Views.UserControls
+{
+    public partial class uc_clients : UserControl
+    {
+        public uc_clients()
+        {
+            InitializeComponent();
+        }
+    }
+}
