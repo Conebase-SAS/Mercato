0ac246fd54e1d07377eff221f8d90778bcb8d083
Lens Mutombo <lensmutombo@gmail.com>
2022-07-04 15:31:18 +0200

    Create frm_approvisionnement.cs

---
+++ Mercato/Mercato/Views/Forms/frm_approvisionnement.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Mercato.Controllers;
+using Mercato.Handlers;
+using Mercato.Models;
+using Mercato.Views.Forms;
+
+namespace Mercato.Views.Forms
+{
+    public partial class frm_approvisionnement : Form
+    {
+        Notification_Center notify = new Notification_Center();
+        clsDepot cls = new clsDepot();
+        Datalib datalib = new Datalib();
+        Depot depot = new Depot();
+        public frm_approvisionnement()
+        {
+            InitializeComponent();
+            refresh();
+        }
+        private void refresh()
+        {
+            cls.charger_depot(cbx_depot);
+            txt_article.Text = "";
+            txt_num_approv.Text = "";
+            txt_points.Text = "";
+            txt_prix_achat_dollars.Text = "";
+            txt_prix_achat_francs.Text = "";
+            txt_prix_solde_dollars.Text = "";
+            txt_prix_solde_fc.Text = "";
+            txt_prix_vente_dollars.Text = "";
+            txt_prix_vente_fc.Text = "";
+            txt_qte_entree.Text = "";
+            txt_search.Text = "";
+            cbx_depot.Text = "";
+            cbx_fournisseur.Text = "";
+            cbx_status.Text = "";
+        }
+        private void bunifuImageButton1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_add_fournisseur_Click(object sender, EventArgs e)
+        {
+            var fr = new frm_fournisseurs();
+            fr.ShowDialog();
+        }
+
+        private void bunifuImageButton2_Click(object sender, EventArgs e)
+        {
+            var fr = new frm_depot();
+            fr.ShowDialog();
+        }
+
+        private void btn_refresh_Click(object sender, EventArgs e)
+        {
+            refresh();
+        }
+    }
+}
