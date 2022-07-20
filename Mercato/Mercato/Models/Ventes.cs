58a323314f438b856987f9d24687decb7c0d4277
Lens Mutombo <lensmutombo@gmail.com>
2022-07-20 15:14:13 +0200

    Create Ventes.cs

---
+++ Mercato/Mercato/Models/Ventes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mercato.Models
+{
+    class Ventes
+    {
+        public int Num_vente { get; set; }
+        public string Vente_id { get; set; }
+        public DateTime Date_vente { get; set; }
+        public string Id_boutique { get; set; }
+        public string Id_clients { get; set; }
+        public string Description_ventes { get; set; }
+    }
+}
